1b1aea16230033a51321aa3e4c8aeaa4710e8fe1
Mike <mlevan1992@gmail.com>
2018-03-23 14:43:28 -0400

    Update Get-SCCMMissingApp.ps1

--- Get-SCCMMissingApp.ps1
+++ Get-SCCMMissingApp.ps1
@@ -1,4 +1,4 @@
-﻿Function Get-SCCMMissingApp {
+Function Get-SCCMMissingApp {
     [cmdletbinding(DefaultParameterSetName = 'SCCMQueryMissingApp', SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
     Param (
         [Parameter(Mandatory = $true,
@@ -13,33 +13,39 @@
         [Parameter(Mandatory = $true,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
-            HelpMessage = 'Please enter the limited collection name for your new device collection',
+            HelpMessage = 'Please enter an application name for your new SCCM collection',
             ParameterSetName = 'SCCMQueryMissingApp')]
         [ValidateNotNullOrEmpty()]
         [Alias('LimitingCollection')]
         [psobject]$LimitingCollectionName,
 
         [Parameter(HelpMessage = 'Please type in a comment/description for your SCCM collection',
-            ParameterSetName = 'SCCMQueryMissingApp')]
+        ParameterSetName = 'SCCMQueryMissingApp')]
         [string]$Comment,
+
+        [Parameter(Mandatory=$true, 
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        HelpMessage='Please type in the appropriate application name that you want to query')]
+        [string]$Application,
 
         [Parameter(HelpMessage = 'The error log is for any errors that occur. They will be stored in the specified location',
             ParameterSetName = 'SCCMQueryMissingApp')]
         [string]$ErrorLog = (Read-Host 'Please enter a UNC path for an error log if an error occurs in your script')
     )
     Begin {   
-        Write-Warning 'Please ensure you have started a PowerShell connection connecting to your appropriate SCCM environment.'
+        Write-Warning 'Please ensure you have started a PowerShell Window connecting to your appropriate SCCM environment.'
     }
 
     Process {
         Try {
             Write-Verbose 'Collecting New-CMDeviceCollection parameters'
             $NewCMDeviceCollectionSPLAT = @{
-                'Name'                   = $CollectionName
+                'Name'               = $CollectionName
                 'LimitingCollectionName' = $LimitingCollectionName
-                'RefreshType'            = 'Manual'
-                'Comment'                = $Comment
-                'Confirm'                = $True
+                'RefreshType'        = 'Manual'
+                'Comment'            = $Comment
+                'Confirm'            = $true
             }
             Write-Verbose 'Creating new device collection'
             $NEWCMDeviceCollection = New-CMDeviceCollection @NewCMDeviceCollectionSPLAT
@@ -64,12 +70,8 @@
                 $AddDeviceCollectionQuerySPLAT = @{
                     'CollectionName'  = $CollectionName
                     'CollectionID'    = $NEWCMDeviceCollection.CollectionID
+                    'QueryExpression' = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System  inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId  where SMS_G_System_COMPUTER_SYSTEM.Name not in  (select distinct SMS_G_System_COMPUTER_SYSTEM.Name  from SMS_R_System  inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId  inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceID = SMS_R_System.ResourceId  where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%$Application%")"
                     'RuleName'        = $RuleName
-                    'QueryExpression' = "select SMS_R_SYSTEM.ResourceID,
-                                    SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,
-                                    SMS_R_SYSTEM.SMSUniqueIdentifier,
-                                    SMS_R_SYSTEM.ResourceDomainORWorkgroup,
-                                    SMS_R_SYSTEM.Client from SMS_R_System  inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId  where SMS_G_System_COMPUTER_SYSTEM.Name not in  (select distinct SMS_G_System_COMPUTER_SYSTEM.Name  from SMS_R_System  inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId  inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceID = SMS_R_System.ResourceId  where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like " % $Application % ")"
                 }
                 $AddDeviceCollectionQuery = Add-CMDeviceCollectionQueryMembershipRule @AddDeviceCollectionQuerySPLAT
     
@@ -83,7 +85,7 @@
         }#TRY
         Catch {
             Write-Warning 'An error has occured. Please review the error logs'
-            $_ | Out-File $ErrorLog
+            $_ | Out-File -Encoding utf8 -FilePath ('FileSystem::' + $ErrorLog)
             #Throw error to host
             Throw
         }
